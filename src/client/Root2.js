3966ed9ba4c2a5e498347a5b829506da297b956a
cadimi <yn1426@gmail.com>
2020-09-22 00:24:41 +0900

    second commit

---
+++ src/client/Root2.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom'
+import App from '../shared/App2';
+import React from "react";
+
+const Root = () => (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
+
+export default Root;
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Menu from '../components/Menu';
 
 const Test = () => {
     return (
-        <div>
-            <h2>Test</h2>
-        </div>
+        <Menu/>
     );
 };
 
